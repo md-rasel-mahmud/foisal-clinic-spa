8f9b83403f18eb0960dadf7a5817a2e803f68355
myself-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2022-12-12 16:35:11 +0600

    new update 3.0

--- src/component/Appointment/DoctorForApmnt.jsx
+++ src/component/Appointment/DoctorForApmnt.jsx
@@ -1,17 +1,20 @@
 
-function DoctorForApmnt() {
+
+
+
+function DoctorForApmnt(props) {
   return (
     <div className="avater shadow text-center bg-secondary ">
-        <img className="rounded-circle w-50 my-4" src="images/dr.belal.jpg" alt=""/>
+        <img className="rounded-circle w-50 my-4" src={props.imgSrc} alt=""/>
 
         <div className="avater-content  pb-4">
-            <h4 className="text-center text-light">Dr. Bellal Hossen</h4>
+            <h4 className="text-center text-light">{props.dName}</h4>
 
-            <p>Medical officer-MBBS, MCPS(sugery), FCPS(surgery)</p>
+            <p>{props.dPosition}</p>
 
-            <a href="tel:+880 1812-000-000"  className="btn bg-color"><i className="fas fa-phone"></i></a>
-            <a href="#" className="btn bg-color"><i className="fas fa-envelope"></i></a>
-            <a href="#" className="btn bg-color"><i className="fab fa-facebook"></i></a>
+            <a href={props.dCall}  className="btn bg-color mx-2"><i className="fas fa-phone"></i></a>
+            <a href={props.dWhatsapp} className="btn bg-color mx-2"><i class="fa-brands fa-whatsapp"></i></a>
+            <a href={props.dFacebook} className="btn bg-color mx-2"><i className="fab fa-facebook"></i></a>
         </div>
     </div>
   );
